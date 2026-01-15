14e399eaee33864464e861cb7bb44dd43577c0c1
RodrigoMaga <rgn.nunnes@gmail.com>
2026-01-15 16:50:09 -0300

    spring

---
+++ src/main/java/com/rodmag/youtube_premium_billing_bot/YoutubePremiumBillingBotApplication.java
@@ -0,0 +1,13 @@
+package com.rodmag.youtube_premium_billing_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubePremiumBillingBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubePremiumBillingBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodmag/youtube_premium_billing_bot/YoutubePremiumBillingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodmag.youtube_premium_billing_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubePremiumBillingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
